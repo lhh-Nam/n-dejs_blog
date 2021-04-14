ff2b6a6f1b4b1807e8175639d8801df1ccd3f6f8
HuyNam <>
2021-04-14 19:42:35 +0700

    add handlebars

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/news", function (req, res) {
+  res.render("news");
+});
+
+app.listen(port, () => console.log(`app listening ${port}`));
